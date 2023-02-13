5f0fa09ee48028d4758535e24a6e2944004c5cbe
bksowmya <sowmyabk3008@gmail.com>
2023-02-13 16:46:28 +0530

    Autehtication app first commit

--- authentication/app/controllers/application_controller.rb
+++ authentication/app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, :only => :create
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :firstname, :lastname, :username) }
+  end
 end
