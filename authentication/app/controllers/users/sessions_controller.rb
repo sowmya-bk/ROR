960b96c4fb2d4aff35e91903e79d8ff7cb34a238
bksowmya <sowmyabk3008@gmail.com>
2023-02-13 11:06:22 +0530

    Adding authentication file

---
+++ authentication/app/controllers/users/sessions_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+  def create
+    user = User.find_by(email: params['email'])
+    if user.valid_password?(params['password'])
+      sign_in(:user, user)
+      redirect_to root_path
+    end
+  end
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+  private
+  def user_params
+    params.permit(:email,:password)
+  end
+end
