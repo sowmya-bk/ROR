8d94981571ebd0e55b8ef18d147a81ab1b37cc71
bksowmya <sowmyabk3008@gmail.com>
2023-02-27 17:06:28 +0530

    Added a new field in post model

--- authentication/app/controllers/posts_controller.rb
+++ authentication/app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :username)
+      params.require(:post).permit(:title, :body, :post_created_user_id)
     end
 end
--- authentication/app/models/post.rb
+++ authentication/app/models/post.rb
@@ -2,5 +2,5 @@ class Post
   include Mongoid::Document
   field :title, type: String
   field :body, type: String
-  field :username, type: String
+  field :post_created_user_id, type: String
 end
