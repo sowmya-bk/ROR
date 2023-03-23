3174a7c0b7a863d538b231a7cef461d0cc71702f
bksowmya <sowmyabk3008@gmail.com>
2023-03-23 17:42:04 +0530

    added timestamps to model

--- authentication/app/models/post.rb
+++ authentication/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps 
   field :title, type: String
   field :body, type: String
   field :post_created_user_id, type: String
--- authentication/app/models/user.rb
+++ authentication/app/models/user.rb
@@ -1,6 +1,7 @@
 class User
  
   include Mongoid::Document
+  include Mongoid::Timestamps 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
